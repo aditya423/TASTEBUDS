01f04ddfeb856b963ea77244e180f98f91adc9ee
Aditya Ghadge <adityasghadge@gmail.com>
2025-06-17 23:15:32 +0530

    Initial Commit

---
+++ TASTEBUDS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TASTEBUDS
+//
+//  Created by Aditya on 17/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
